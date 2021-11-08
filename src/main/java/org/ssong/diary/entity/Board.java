17575d2a4c3d9b34d98c8272d519ca73e44153d9
songyikim0911 <ksi6470@gmail.com>
2021-11-08 18:20:09 +0900

    Initial commit

---
+++ src/main/java/org/ssong/diary/entity/Board.java
@@ -0,0 +1,42 @@
+package org.ssong.diary.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    @CreationTimestamp
+    private LocalDateTime regDate;
+
+    @UpdateTimestamp
+    private LocalDateTime modDate;
+
+    public void change(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
