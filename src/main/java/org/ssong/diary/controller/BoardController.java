bb40e28c94c3a267d7dbb28baf1ed3960bb07fb9
songyikim0911 <ksi6470@gmail.com>
2021-11-09 23:02:26 +0900

    thymeleaf 추가

---
+++ src/main/java/org/ssong/diary/controller/BoardController.java
@@ -0,0 +1,48 @@
+package org.ssong.diary.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.ssong.diary.dto.BoardDTO;
+import org.ssong.diary.dto.PageRequestDTO;
+import org.ssong.diary.service.BoardService;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        model.addAttribute("responseDTO",boardService.getList(pageRequestDTO));
+
+
+    }
+
+    @GetMapping("/register")
+    public void registerGet(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO boardDTO, RedirectAttributes redirectAttributes){
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+
+    }
+
+
+}
